de984bf53ddfa9e68f2dfbd32dc10e0f896461ce
david11yf29 <david11yf29@gmail.com>
2020-08-23 16:19:43 +0800

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor
+}
+
+// ProjectInput class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -27,6 +41,7 @@ class ProjectInput {
     this.attach();
   }
 
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     // console.log("1", this);
@@ -37,7 +52,7 @@ class ProjectInput {
   private configure() {
     // console.log("2", this);
     // this 指 ProjectInput
-    this.element.addEventListener('submit', this.submitHandler.bind(this));
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
